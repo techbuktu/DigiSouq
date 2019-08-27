4af114add16c84dc9137e93e883501c3e35539db
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-27 09:40:38 -0400

    Setup SQLite3 DB and ran initial migrations for project's default apps

--- digisouq/settings/main.py
+++ digisouq/settings/main.py
@@ -70,7 +70,7 @@ WSGI_APPLICATION = 'digisouq.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db/digisouq.sqlite3'),
     }
 }
 
