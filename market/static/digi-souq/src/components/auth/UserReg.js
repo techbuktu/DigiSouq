fdca02e2556c312847314fedabfd2d125998b3a2
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-27 10:38:33 -0400

    Added a UserReg component to handle User registration

---
+++ market/static/digi-souq/src/components/auth/UserReg.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import UserApi from '../../api/UserApi';
+
+class UserReg extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default UserReg;
