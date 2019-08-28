525a939e8f544b8bf8a79432d1976b22645745ee
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-28 18:56:36 -0400

    Registered all Market models with the Django AdminSite

--- market/admin.py
+++ market/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from market.models import (
+    Seller, Buyer, Product, Bid
+)
 # Register your models here.
+admin.site.register(Seller, admin.ModelAdmin)
+admin.site.register(Buyer, admin.ModelAdmin)
+admin.site.register(Product, admin.ModelAdmin)
+admin.site.register(Bid, admin.ModelAdmin)
