3261cdf4a30ea20064e32919999d70af132a7309
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-28 19:06:11 -0400

    UserApi implemented!

--- market/static/digi-souq/src/api/UserApi.js
+++ market/static/digi-souq/src/api/UserApi.js
@@ -1,8 +1,17 @@
 import axios from 'axios';
 
 class UserApi {
-    constructor(){}
+    constructor(){
+        this.apiUrl = "localhost:8000/api/market/";
+    }
     
+    newUser(user_obj){
+        return axios.post("localhost:8000/api/market/users", user_obj);
+    }
+
+    getUser(pk){
+        return axios.get(`${this.apiUrl}/${pk}`);
+    }
 }
 
 
