7b465dda3b79f1d1a0490018fea6eefeb49b8c52
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-30 18:21:35 -0400

    Generated auth tokens for existing users

--- market/models.py
+++ market/models.py
@@ -99,7 +99,7 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False **kwargs):
+def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
         Token.objects.create(user=instance)
 
