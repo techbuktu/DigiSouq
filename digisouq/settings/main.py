1273bc48efcf5f2684e4374295edc7f5f4ab33ac
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-30 16:33:24 -0400

    Setup settings.AUTH_USER_MODEL

--- digisouq/settings/main.py
+++ digisouq/settings/main.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+AUTH_USER_MODEL = 'auth.User'
+
 STATIC_URL = '/static/'
 
 #Django REST Framework configurations
