2bc8fb2115f84b3c832a66bdce7130f938b1835d
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-31 15:08:24 -0400

    Setup UserSignIn component

--- market/static/digi-souq/src/components/auth/UserSignIn.js
+++ market/static/digi-souq/src/components/auth/UserSignIn.js
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import UserApi from '../../api/UserApi';
 
 class UserSignIn extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        };
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
 
     submitForm() {
         console.log("SignIn form submitted");
@@ -11,18 +21,18 @@ class UserSignIn extends Component {
         return (
             <div>
                 Sign in to Your DigiSouq account.
-                <form onSubmit = {this.submitForm()}>
+                <form onSubmit = {this.submitForm}>
                     <div>
                         <label>
                             Username
                         </label>
-                        <input type="text" name="username"/>
+                        <input type="text" name="username" defaultValue="" onChange={this.onChange}/>
                     </div>
                     <div>
                         <label>
                             Password
                         </label>
-                        <input type="password" name="password"/>
+                        <input type="password" name="password" defaultValue="" onChange={this.onChange}/>
                     </div>
                     <div>
                         <input type="submit" value="Sign In"/>
