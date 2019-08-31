5f88d6f0eeba77b8fe5d05103651ab4374428a67
Muhammad Jalloh <dev@TechBuktu.com>
2019-08-31 01:23:49 -0400

    Successful implementation of form handling, setState and API POSTing in UserReg component!

--- market/static/digi-souq/src/components/auth/UserReg.js
+++ market/static/digi-souq/src/components/auth/UserReg.js
@@ -6,26 +6,72 @@ class UserReg extends Component {
     constructor(props){
         super(props);
         this.state = {
-            first_name: 'Muhammad',
+            UserJson: '',
+            first_name: '',
             last_name: '',
             username: '',
             password: ''
         };
 
+
+
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+
     }
+
     componentDidMount(){
         console.log(this.state);
         UserApi.getUser(1)
             .then(res => { console.log(res)});
     }
 
-    onChange(e) {
-        this.setState({[e.target.name] : e.target.data })
-        //console.log("onChange() this.state:" + this.state);
-        console.log(this.state.first_name);
+    onChange(e){
+        this.setState({ [e.target.name]: e.target.value }, () => {
+            
+            this.createNewUserObj();
+        });
+       
+        
     }
+
+    createNewUserObj(){
+        const newUser = {
+            first_name: this.state.first_name,
+            last_name : this.state.last_name,
+            username: this.state.username,
+            password: this.state.password
+        };
+
+        console.log("newUser.last_name obj: " + newUser.last_name);
+        const newUserJson = JSON.stringify(newUser);
+        console.log("newUserJson obj: " + newUserJson);
+
+        this.setState({
+            UserJson: newUserJson
+        })
+
+        console.log('this.state.UserJson: ' + this.state.UserJson);
+    }
+
+    postNewUser(){
+
+        UserApi.newUser(this.state.UserJson)
+            .then(response => {
+                console.log(response);
+            })
+            .catch(err => {
+                console.log("Error Message:" + err);
+            })
+            .finally(() => {
+                console.log("UserApi.newUser() ran...");
+
+            });
+
+
+    }
+
+
 
     onSubmit(e){
         e.preventDefault();
@@ -35,37 +81,9 @@ class UserReg extends Component {
         console.log("this.state.username:" + this.state.username);
         console.log("this.state.password:" + this.state.password);
 
-        const newUser = {
-            first_name: this.state.first_name,
-            last_name : this.state.last_name,
-            username: this.state.username,
-            password: this.state.password
-        }
+        this.createNewUserObj();
 
-        console.log("newUser.first_name obj: " + newUser.first_name);
-        const UserJson = JSON.stringify(newUser);
-        console.log(UserJson);
-
-        /*
-        const testUser = {
-            first_name: 'Allison',
-            last_name: 'Wondersome',
-            username: 'nomadist',
-            password: 'Secret123'
-        }
-        let testJson = JSON.stringify(testUser);
-        */
-
-        UserApi.newUser(UserJson)
-            .then(response => {
-                console.log(response);
-            })
-            .catch(err => {
-                console.log("Error Message:" + err);
-            })
-            .finally(() => {
-                console.log("UserApi.newUser() ran...");
-            })
+        this.postNewUser();
 
     }
 
@@ -77,22 +95,22 @@ class UserReg extends Component {
                     <label>
                         First Name
                     </label>
-                    <input type="text" name="first_name" onChange={this.OnChange} defaultValue={this.state.first_name} data={this.state.first_name} />
+                    <input type="text" name="first_name" onChange={this.onChange} defaultValue={this.state.first_name} data={this.state.first_name} />
                     <br/>
                     <label>
                         Last Name
                     </label>
-                    <input type="text" name="last_name" onChange={this.OnChange} data={this.state.last_name}  defaultValue={this.state.last_name} />
+                    <input type="text" name="last_name" onChange={this.onChange} data={this.state.last_name}  defaultValue={this.state.last_name} />
                     <br/>
                     <label>
                         Username 
                     </label>
-                    <input type="text" name="username" onChange={this.OnChange} data={this.state.username}  defaultValue={this.state.username} />
+                    <input type="text" name="username" onChange={this.onChange} data={this.state.username}  defaultValue={this.state.username} />
                     <br/>
                     <label>
                         Password
                     </label>
-                    <input type="password" name="password" onChange={this.OnChange} data={this.state.password}  defaultValue={this.state.password} />
+                    <input type="password" name="password" onChange={this.onChange} data={this.state.password}  defaultValue={this.state.password} />
                     <br/>
                     <input type="submit" value="Register"/>
                 </form>
