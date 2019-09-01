43f96a259c5178ef4e57b5a2d387541296ad4690
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-01 00:21:02 -0400

    ProductApi implemented

--- market/static/digi-souq/src/api/ProductApi.js
+++ market/static/digi-souq/src/api/ProductApi.js
@@ -3,6 +3,23 @@ import Axios from './BaseApi';
 
 class ProductApi {
     constructor(){}
+
+    static getAllProducts(){
+        return Axios.get(`/products/`);
+    }
+
+    static getSingleProduct(product_link){
+        return Axios.get(`/products/${product_link}/`);
+    }
+
+    static newProduct(product_obj){
+        return Axios.post(`/products/`, product_obj);
+    }
+
+    static updateProduct(product_link, updated_product){
+        return Axios.put(`/products/${product_link}/`, updated_product);
+    }
+
 }
 
 export default ProductApi;
