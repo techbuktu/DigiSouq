f64d9aa71000841f97b6e08ac9961e063eb04630
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-01 00:12:00 -0400

    BidApi.placeBid()

--- market/static/digi-souq/src/api/BidApi.js
+++ market/static/digi-souq/src/api/BidApi.js
@@ -1,10 +1,15 @@
 import Axios from './BaseApi';
 
 class BidApi {
+
+
     static getBidsByBuyer(buyer_link){
         return Axios.get(`/bids/${buyer_link}`);
     }
 
+    static placeBid(bid_obj){
+        return Axios.post(`/bids/`, bid_obj);
+    }
 }
 
 export default BidApi;
