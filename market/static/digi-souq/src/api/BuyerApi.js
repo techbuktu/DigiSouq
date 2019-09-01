3766de4d36ae1d5e1edb4bc1dfa102fc2d52e8db
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-01 00:16:53 -0400

    BuyerApi implemented

--- market/static/digi-souq/src/api/BuyerApi.js
+++ market/static/digi-souq/src/api/BuyerApi.js
@@ -1,7 +1,23 @@
 import Axios from './BaseApi';
 
 class BuyerApi {
-    constructor()
+    constructor(){}
+
+    static getBuyerDetails(buyer_link){
+        return Axios.get(`/buyers/${buyer_link}/`);
+    }
+
+    static newBuyer(buyer_obj){
+        return Axios.post(`/buyers/`, buyer_obj);
+    }
+
+    static updateBuyer(buyer_link, updated_buyer_obj){
+        return Axios.put(`/buyers/${buyer_link}/`, updated_buyer_obj);
+    }
+
+    static getAllBuyers(){
+        return Axios.get(`/buyers/`);
+    }
 }
 
 
