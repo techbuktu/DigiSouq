833a5019e31ab3c418e26380715cb56599e27556
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-01 00:25:40 -0400

    Implemented: SellerApi

--- market/static/digi-souq/src/api/SellerApi.js
+++ market/static/digi-souq/src/api/SellerApi.js
@@ -1,7 +1,27 @@
 import Axios from './BaseApi';
 
 class SellerApi {
-    constructor()
+    constructor(){
+
+    }
+
+    static getAllSellers(){
+        returnAxios.get(`/sellers/`);
+    }
+
+    static getSeller(seller_link){
+        return Axios.get(`/sellers/${seller_link}`);
+    }
+
+    static newSeller(seller_obj){
+        return Axios.post(`/sellers/`, seller_obj);
+    }
+
+    static updateSeller(seller_link, updated_seller_obj){
+        return Axios.put(`/sellers/${seller_link}/`, updated_seller_obj);
+    }
+
+
 }
 
 
