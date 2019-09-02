221a944e9b63922b6d3828d618f2fb761eedcd6d
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-02 16:16:56 -0400

    BidApi.getBidsforSeller() added

--- market/static/digi-souq/src/api/BidApi.js
+++ market/static/digi-souq/src/api/BidApi.js
@@ -4,11 +4,15 @@ class BidApi {
 
 
     static getBidsByBuyer(buyer_link){
-        return Axios.get(`/bids/${buyer_link}`);
+        return Axios.get(`/bids/?buyer=${buyer_link}`);
     }
 
     static placeBid(bid_obj){
         return Axios.post(`/bids/`, bid_obj);
+    }
+
+    getBidsforSeller(seller_link){
+        return Axios.get(`/bids/?seller=${seller_link}`);
     }
 }
 
