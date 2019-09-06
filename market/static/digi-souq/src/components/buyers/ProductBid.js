ae69fd8b738e056ccffd32f127626aaa28fd215d
Muhammad Jalloh <dev@TechBuktu.com>
2019-09-06 16:52:04 -0400

    Setup structure and logic of ProductBid component

--- market/static/digi-souq/src/components/buyers/ProductBid.js
+++ market/static/digi-souq/src/components/buyers/ProductBid.js
@@ -8,10 +8,57 @@ class ProductBid extends Component {
 
         let token = localStorage.getItem('auth_token');
         this.state = {
-            auth_token: token
+            auth_token: token,
+            productLink: '',
+            product: {},
+            productBid : {}
         }
+
+        this.onChange = this.onChange.bind(this);
+        this.placeBid = this.placeBid.bind(this);
     }
 
+    componentWillMount(){
+        let product_link = this.props.match.params.productLink;
+        this.setState({
+            productLink: product_link
+        }, () => {
+            this.getProduct();
+        })
+        
+    }
+
+    getProduct(){
+        ProductApi.getSingleProduct(this.state.productLink)
+            .then(response => {
+                this.setState({
+                    product: response.data
+                }, () => {
+                    console.log(`getSingProduct() API Response: ${this.state.product}`);
+                })
+            })
+            .catch(err => {
+                console.log(`API error msg: ${err}`);
+            })
+            .finally()
+    }
+
+    onChange(e){
+        this.setState({
+            [e.target.name]: e.target.value
+        }, () => {
+            this.createBidObject();
+        })
+    }
+
+    createBidObject(){
+        return;
+    }
+
+    placeBid(e){
+        e.preventDefault();
+        //POST Bid 
+    }
     
     render() {
         if(this.state.auth_token){
